2411df11c456689fdf3aa5b51916c2428d8ded76
fqian <francois.qian2@gmail.com>
2025-10-08 18:14:33 +0100

    uh

--- flake.nix
+++ flake.nix
@@ -3,13 +3,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    nvf.url = "github:notashelf/nvf";
-    nvf.inputs.nixpkgs.follows = "nixpkgs";
+    nvim-config.url = "path:./nvim";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, disko, nvf, ... }:
+  outputs = { self, nixpkgs, home-manager, disko, ... }@inputs:
     let
       system = "x86_64-linux";
     in {
@@ -19,12 +18,15 @@
           disko.nixosModules.disko
           ./configuration.nix
           ./disk-config.nix
-          nvf.homeManagerModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.fqian = import ./home.nix;
+            home-manager.users.fqian = {
+              imports = [
+	        ./home.nix
+              ];
+            };
             home-manager.backupFileExtension = "backup";
           }
         ];
