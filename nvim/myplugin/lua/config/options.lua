b2af5ffd615f106c0316a57118c4a6c5edc9a164
fqian <francois.qian2@gmail.com>
2025-10-13 06:21:36 +0100

    nvim touchy

--- nvim/myplugin/lua/config/options.lua
+++ nvim/myplugin/lua/config/options.lua
@@ -56,8 +56,8 @@ vim.opt.foldmethod = syntax -- Use treesitter fold method
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()' -- Use treesitter for folding
 vim.opt.foldenable = true -- Enable folds
 vim.opt.foldlevelstart = 9 -- Start 3 folds deep
-vim.opt.foldopen =
-vim.opt.wildignore = { '*.o', '*.obj', '*.bin', '*.dll', '*.exe' } -- Ignore common binary files during tab completion
+vim.opt.foldopen = ''
+vim.opt.wildignore:append { '*.o', '*.obj', '*.bin', '*.dll', '*.exe' } -- Ignore common binary files during tab completion
 vim.cmd('syntax on') -- Enable syntax highlighting
 -- vim.cmd('colorscheme desert')          -- Set the colorscheme to 'desert'
 vim.opt.winblend = 0
