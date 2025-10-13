6e2266a60dce7e740f3500254ff1d7b1b9293e98
fqian <francois.qian2@gmail.com>
2025-10-13 02:16:47 +0100

    f

---
+++ nvim/myplugin/lua/config/conform.lua
@@ -0,0 +1,11 @@
+require("conform").setup({
+    formatters_by_ft = {
+      lua = {'stylua'},
+      rust = {'rustfmt', lsp_format = "fallback" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = true,
+    },
+  },
+}})
