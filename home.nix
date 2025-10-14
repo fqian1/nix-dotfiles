735745f257286b49759c7636e3d318affe1e8415
fqian <francois.qian2@gmail.com>
2025-10-14 03:17:33 +0100

    auto: 2025-10-14_03:17:33

--- home.nix
+++ home.nix
@@ -81,6 +81,7 @@ in {
     settings = {
       font_family = "FiraCode Nerd Font";
       font_size = 12.0;
+      theme = "kanagawa-dragon";
       background_opacity = "0.8";
       shell = "${pkgs.fish}/bin/fish";
     };
