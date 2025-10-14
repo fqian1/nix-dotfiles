ee9b38d00995c064df94ad0692b5b8ecef2b8adc
fqian <francois.qian2@gmail.com>
2025-10-14 09:54:24 +0100

    auto: 2025-10-14_09:54:24

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["ntfs"];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
