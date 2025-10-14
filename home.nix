98bec6a7a305a890353955be8922f13821d043e4
fqian <francois.qian2@gmail.com>
2025-10-14 15:11:55 +0100

    auto: 2025-10-14_15:11:55

--- home.nix
+++ home.nix
@@ -41,39 +41,12 @@ in {
 
   programs.starship = {
     enable = true;
-    enableFishIntegration = true;
     settings = {
-      add_newline = false;
+      add_newline = true;
       command_timeout = 500;
       continuation_prompt = "[∙](bright-black) ";
-      format = ''
-        [](color_orange)\
-        $os\
-        $username\
-        [](bg:color_yellow fg:color_orange)\
-        $directory\
-        [](fg:color_yellow bg:color_aqua)\
-        $git_branch\
-        $git_status\
-        [](fg:color_aqua bg:color_blue)\
-        $c\
-        $cpp\
-        $rust\
-        $golang\
-        $nodejs\
-        $php\
-        $java\
-        $kotlin\
-        $haskell\
-        $python\
-        [](fg:color_blue bg:color_bg3)\
-        $docker_context\
-        $conda\
-        $pixi\
-        [](fg:color_bg3 bg:color_bg1)\
-        $time\
-        [ ](fg:color_bg1)\
-        $line_break$character'';
+      format = "[](0x9A348E)$username$hostname$localip$shlvl$singularity$kubernetes[](fg:0x9A348E bg:0xDA627D)$directory$vcsh[](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch[](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf[](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery[](fg:0x06969A bg:0x33658A)$time$status$shell$character";
+      right_format = "";
       scan_timeout = 30;
 
       aws = {
@@ -423,7 +396,7 @@ in {
         deleted = "🗑";
         disabled = false;
         diverged = "😵";
-        format = "([[$all_status$ahead_behind]]($style) )";
+        format = "([$all_status$ahead_behind]($style) )";
         ignore_submodules = false;
         modified = "📝";
         renamed = "👅";
