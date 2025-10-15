e3eef60a92d64542fbf94376919569744f0ca476
fqian <francois.qian2@gmail.com>
2025-10-15 11:51:45 +0100

    auto: 2025-10-15_11:51:45

--- home.nix
+++ home.nix
@@ -517,8 +517,8 @@ in {
     initExtra = ''
       set -o vi
       set show-mode-in-prompt on
-      set vi-ins-mode-string "\1\e[6 q\2"
-      set vi-cmd-mode-string "\1\e[2 q\2"
+      set vi-cmd-mode-string "\1\e[2 q\2cmd"
+      set vi-ins-mode-string "\1\e[6 q\2ins"
     '';
     shellAliases = {
       nrs = "sudo nixos-rebuild switch --flake ~/.dotfiles/#nixos";
