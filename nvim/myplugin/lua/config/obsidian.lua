0ccdeb08b8bb391d42cd55bd4cb6472c0c01107f
fqian <francois.qian2@gmail.com>
2025-10-17 08:07:17 +0100

    auto: 2025-10-17_08:07:17

--- nvim/myplugin/lua/config/obsidian.lua
+++ nvim/myplugin/lua/config/obsidian.lua
@@ -1,1 +1,32 @@
-require('obsidian').setup()
+require("obsidian").setup({
+    workspaces = {
+        {
+            name = "main", -- The name for this workspace
+            path = "~/projects/obsidian-vault", -- The path to your vault
+        },
+    },
+
+    -- OPTIONAL: Add common options here
+    -- For example, to set up keybindings:
+    -- mappings = {
+    --     -- Normal mode
+    --     ["gf"] = {
+    --         action = function()
+    --             return require("obsidian").util.split_or_open_link()
+    --         end,
+    --         opts = { silent = true, noremap = true },
+    --     },
+    -- },
+
+    -- To enable daily notes and set a preferred file format:
+    -- daily_notes = {
+    --     folder = "daily",
+    --     date_format = "%Y-%m-%d", -- e.g., 2023-10-17
+    --     alias_format = "%A, %B %d, %Y", -- e.g., Thursday, October 17, 2025
+    -- },
+
+    -- To set the template path
+    -- templates = {
+    --     subdir = "templates",
+    -- },
+})
