9de85c7deedd2fb7f1565818f52efcc22328f9ee
fqian <francois.qian2@gmail.com>
2025-10-19 08:15:37 +0100

    auto: 2025-10-19_08:15:37

--- home.nix
+++ home.nix
@@ -589,7 +589,7 @@ in {
 
       fedit() {
         local file_to_edit
-        file_to_edit=$(fd --type f "$@" "." "$HOME/.dotfiles" | fzy)
+        file_to_edit=$(fd $@ . ~/.dotfiles -tf | fzy)
 
         if [[ -n "$file_to_edit" ]]; then
           vim "$file_to_edit"
@@ -601,26 +601,32 @@ in {
         if [[ $# -eq 1 ]]; then
             selected=$1
         else
-            selected=$(fd ~/work ~/projects --exact-depth 1 -td; echo ~/.dotfiles | fzy)
+            selected=$( (fd . ~/work ~/projects --exact-depth 1 -td; echo ~/.dotfiles) | fzy)
         fi
 
         if [[ -z $selected ]]; then
-            exit 0
+            return 0
         fi
 
         selected_name=$(basename "$selected" | tr . _)
         tmux_running=$(pgrep tmux)
 
-        if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-            tmux new-session -s $selected_name -c $selected
-            exit 0
+        if [[ -z $TMUX ]]; then
+            # Not inside tmux
+            if tmux has-session -t="$selected_name" 2>/dev/null; then
+                tmux attach-session -t "$selected_name"
+            else
+                tmux new-session -s "$selected_name" -c "$selected"
+            fi
+        else
+            # Inside tmux
+            if tmux has-session -t="$selected_name" 2>/dev/null; then
+                tmux switch-client -t "$selected_name"
+            else
+                tmux new-session -ds "$selected_name" -c "$selected"
+                tmux switch-client -t "$selected_name"
+            fi
         fi
-
-        if ! tmux has-session -t=$selected_name 2> /dev/null; then
-            tmux new-session -ds $selected_name -c $selected
-        fi
-
-        tmux switch-client -t $selected_name
       }
       bind -x '"\C-f":f'
     '';
