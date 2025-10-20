c6cdc4f84d44369ee6c625c9407a323cb5a4ee60
fqian <francois.qian2@gmail.com>
2025-10-20 20:07:09 +0100

    auto: 2025-10-20_20:07:09

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -148,12 +148,11 @@
     initialPassword = "password";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you accidentally delete configuration.nix.
