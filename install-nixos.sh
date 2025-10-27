84bae381f6cc7453e7601646d44b7c4306540497
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-27 01:51:23 +0000

    Fix path to disk-config.nix in install script

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /hosts/nixos/disk-config.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./hosts/nixos/disk-config.nix
 nixos-install --flake .#nixos
