2fd80d7c2177e1914085982b5f9e915feeefcac1
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-27 21:18:57 +0000

    Remount Nix store with increased size

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo mount -o remount,size=24G,noatime /nix/.rw-store
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
