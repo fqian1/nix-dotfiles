15b52c762e39c0fe45d4c8c1130cc7dd47d61f4a
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-28 04:52:20 +0000

    Update nix command to write EFI boot entries

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo mount -o remount,size=24G,noatime /nix/.rw-store
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
+sudo nix --experimental-features "nix-command flakes" run --mode mount --write-efi-boot-entries 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
