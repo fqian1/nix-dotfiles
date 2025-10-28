a121eb66c1d0d845da1e8697646879f25de11c2a
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-28 07:28:07 +0000

    Use zram for swap instead of swapfile
    Replaced swapfile creation with zram setup for swap.

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo mount -o remount,size=24G,noatime /nix/.rw-store
-sudo fallocate -l 16G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-swapon --show
+sudo modprobe zram
+sudo zramctl /dev/zram0 --algorithm zstd --size 8G
+sudo mkswap /dev/zram0
+sudo swapon --priority 100 /dev/zram0
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
