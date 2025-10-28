df6d7801bf7431f79a5e66210be253f0a64b55a0
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-28 07:15:17 +0000

    Create and enable a 16G swap file
    Add swap file creation and activation to the script

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo mount -o remount,size=24G,noatime /nix/.rw-store
+sudo fallocate -l 16G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+swapon --show
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
