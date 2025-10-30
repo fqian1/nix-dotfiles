88416d39d4c472bdc5de9901fd2db40aec1b6626
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-30 07:16:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /hosts/nixos/disk-config.nix
-destination="/mnt/home/.dotfiles/";
-sudo cp -a ./. "${destination}/";
-sudo nixos-install --flake "${destination}#nixos" --show-trace;
+sudo nixos-install --no-write-lock-file --root /mnt --flake .#nixos 
