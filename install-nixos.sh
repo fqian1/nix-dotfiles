364cd9cf1e0febbb87d4d4f89623aeb325082c6b
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-30 06:39:04 +0000

    Change disko-install command to use mount mode

--- install-nixos.sh
+++ install-nixos.sh
@@ -4,4 +4,4 @@ sudo modprobe zram
 sudo zramctl /dev/zram0 --algorithm zstd --size 8G
 sudo mkswap /dev/zram0
 sudo swapon --priority 100 /dev/zram0
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --flake .#nixos --disk main /dev/sda
+sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --mode mount --flake .#nixos --disk main /dev/sda
