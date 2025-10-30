09d9c91164c4e2361906a461e824e893fc485385
Francois Qian <114959032+fqian1@users.noreply.github.com>
2025-10-30 16:37:38 +0000

    Delete install-nixos.sh

--- install-nixos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo mount -o remount,size=24G,noatime /nix/.rw-store
-sudo modprobe zram
-sudo zramctl /dev/zram0 --algorithm zstd --size 8G
-sudo mkswap /dev/zram0
-sudo swapon --priority 100 /dev/zram0
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko#disko-install' -- --mode mount --flake .#nixos --disk main /dev/sda
