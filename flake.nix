281bf452c9af9ee88ea70bfb5929e43271235708
fqian <francois.qian2@gmail.com>
2025-11-01 22:56:25 +0000

    auto: 2025-11-01_22:56:25

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "fqian's nixos configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,8 +1,107 @@
-{ lib, ... }:
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./hardware.nix
   ];
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modification
+      outputs.overlays.unstable-packages
+    ];
+    config.allowUnfree = true;
+  };
+
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        experimental-features = "nix-command flakes";
+        flake-registry = "";
+        nix-path = config.nix.nixPath;
+        trusted-users = [
+          "root"
+          "fqian"
+        ];
+      };
+      channel.enable = false;
+
+      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    };
+
+  security = {
+    sudo = {
+      enable = true;
+    };
+    polkit = {
+      enable = true;
+    };
+  };
+
+  services = {
+    fwupd = {
+      enable = true;
+    };
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    # keyMap = "uk";
+    useXkbConfig = true;
+  };
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ALL = "en_GB.UTF-8";
+      LANGUAGE = "en_US.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+    supportedLocales = [
+      "en_GB.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
+  };
+
+  time.timeZone = "Europe/London";
+
+  programs.bash.enable = true;
+
+  environment.variables.EDITOR = "nvim";
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.fira-code
+    ];
+  };
+
+  users.users.fqian = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    initialPassword = "password";
+    shell = pkgs.bash;
+    # openssh.authorizedKeys.keys = [ ];
+  };
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   networking = {
     hostName = "nixos";
@@ -14,6 +113,14 @@
       "1.0.0.1"
     ];
     useDHCP = lib.mkForce true;
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+    };
   };
 
   system.stateVersion = "25.05";
