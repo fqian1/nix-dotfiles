14feb7b1ddbbb1e5070fd28ea89a827980bea3dd
fqian <francois.qian2@gmail.com>
2025-11-07 21:26:46 +0000

    auto: 2025-11-07_21:26:46

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,6 +5,23 @@
 
   # Overrides for existing pkgs
   modifications = final: prev: {
+    dwl = prev.dwl.overrideAttrs (oldAttrs: {
+      pname = "dwl";
+
+      patches = [
+        ./patches/alwayscenter.patch
+        ./patches/autostart.patch
+        ./patches/betterresize.patch
+        ./patches/controlledfullscreen.patch
+        ./patches/ipc.patch
+        ./patches/systemd.patch
+        ./patches/unclutter.patch
+      ];
+
+      postPatch = ''
+        cp ${./config.h} config.h
+      '';
+    });
   };
 
   # Add the option to use unstable via pkgs.unstable.<package>
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 pkgs: {
   nvim = pkgs.callPackage ./nvim { };
   neowall = pkgs.callPackage ./neowall { };
-  dwl = pkgs.callPackage ./dwl { };
 }
