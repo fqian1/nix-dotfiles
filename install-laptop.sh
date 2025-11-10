a5a6687cbb4c2f3c5288531099b3c84279837499
Francois Qian <fqian@Francoiss-MacBook-Pro.local>
2025-11-10 22:23:58 +0000

    add swap to install

--- install-laptop.sh
+++ install-laptop.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+sudo mount -o remount,size=24G,noatime /nix/.rw-store
+sudo modprobe zram
+sudo zramctl /dev/zram0 --algorithm zstd --size 8G
+sudo mkswap /dev/zram0
+sudo swapon --priority 100 /dev/zram0
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount hosts/nixos-laptop/disk-config.nix
 sudo nixos-install --no-write-lock-file --root /mnt --flake .#nixos-laptop
