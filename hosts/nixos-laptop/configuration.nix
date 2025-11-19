2ff6de6ae86fe488772ef84fa26f03148803ad84
fqian <francois.qian2@gmail.com>
2025-11-19 10:59:37 +0000

    auto

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -8,14 +8,16 @@
 }:
 {
   imports = [
-    # outputs.nixosModules.vpn
     outputs.nixosModules.audio
-    outputs.nixosModules.locale
-    outputs.nixosModules.dwl
     outputs.nixosModules.greetd
     outputs.nixosModules.networking
     outputs.nixosModules.impermanence
-    outputs.nixosModules.silent-boot
+    outputs.nixosModules.nix-ld
+    # outputs.nixosModules.silent-boot
+    outputs.nixosModules.locale
+    # outputs.nixosModules.vpn
+    outputs.nixosModules.dwl
+    outputs.nixosModules.ssh
     ./hardware.nix
   ];
 
@@ -51,21 +53,6 @@
     fontconfig.enable = true;
     fontDir.enable = true;
   };
-
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_ALL = "en_GB.UTF-8";
-      LANGUAGE = "en_US.UTF-8";
-      LC_TIME = "en_GB.UTF-8";
-    };
-    supportedLocales = [
-      "en_GB.UTF-8/UTF-8"
-      "en_US.UTF-8/UTF-8"
-    ];
-  };
-
-  time.timeZone = "Europe/London";
 
   users.users.fqian = {
     isNormalUser = true;
--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,31 +1,38 @@
 { lib, ... }:
 {
   networking = {
-    wireless = {
-      enable = true;
-      networks = {
-        "VM3764440" = {
-          psk = "wtw3mzywDbNw";
-        };
-        "iPhone 13 Pro" = {
-          psk = "reps4jesus";
-        };
-        "ASK4 Wireless (802.1x)" = {
-          auth = ''
-            key_mgmt=WPA-EAP
-            identity="fqian"
-            password="amount-strong-got-electric"
-            phase2="auth=MSCHAPV2"
-          '';
-        };
-        "ASK4 Wireless" = {
-          psk = "amount-strong-got-electric";
-        };
-      };
-    };
+    networkmanager.enable = true;
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
     ];
   };
+  # networking = {
+  #   wireless = {
+  #     enable = true;
+  #     networks = {
+  #       "VM3764440" = {
+  #         psk = "wtw3mzywDbNw";
+  #       };
+  #       "iPhone 13 Pro" = {
+  #         psk = "reps4jesus";
+  #       };
+  #       "ASK4 Wireless (802.1x)" = {
+  #         auth = ''
+  #           key_mgmt=WPA-EAP
+  #           identity="fqian"
+  #           password="amount-strong-got-electric"
+  #           phase2="auth=MSCHAPV2"
+  #         '';
+  #       };
+  #       "ASK4 Wireless" = {
+  #         psk = "amount-strong-got-electric";
+  #       };
+  #     };
+  #   };
+  #   nameservers = [
+  #     "1.1.1.1"
+  #     "1.0.0.1"
+  #   ];
+  # };
 }
